d5fddb8151fd7cf098e53b26edb2ab8cfff23dae
Cao Nhat Long <longcnhe141293@gmail.com>
2024-12-06 18:15:13 +0700

    test commit

--- app/src/main/java/com/prm392/group1/apporderfood/activity/MainActivity.java
+++ app/src/main/java/com/prm392/group1/apporderfood/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.prm392.group1.apporderfood;
+package com.prm392.group1.apporderfood.activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.prm392.group1.apporderfood.R;
 
 public class MainActivity extends AppCompatActivity {
 
